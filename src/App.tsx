2f73aedfa3dcce36838ad332360b2e3cb55e4234
Marco Amorim <marcoamorim95@hotmail.com>
2021-06-24 21:20:10 -0300

    working on countdown timer

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { allKeywords } from './data';
 const App = () => {
 	const [currentWord, setCurrentWord] = useState<string>('');
 	const [correctWords, setCorrectWords] = useState<string[]>([]);
+	const [isPaused, setIsPaused] = useState<boolean>(false);
+	const [timeLeft, setTimeLeft] = useState<number>(10);
 
 	useEffect(() => {
 		if (
@@ -16,7 +18,13 @@ const App = () => {
 			setCorrectWords([...correctWords, currentWord]);
 			setCurrentWord('');
 		}
-	}, [correctWords, currentWord]);
+
+		if (!isPaused) {
+			if (timeLeft > 0) {
+				setTimeLeft(timeLeft - 1);
+			}
+		}
+	}, [correctWords, currentWord, isPaused, timeLeft]);
 
 	interface PercentageCalculation {
 		keywordsAmount: number;
@@ -31,8 +39,21 @@ const App = () => {
 		).toFixed();
 	};
 
+	const calculateTimeLeft = (timeLeft: number) => {
+		const minutes = Math.floor(timeLeft / 60);
+		const seconds = timeLeft - minutes * 60;
+
+		return `${minutes < 10 ? '0' + minutes : minutes}:${
+			seconds < 10 ? '0' + seconds : seconds
+		}`;
+	};
+
 	return (
 		<div className="game">
+			<div className="current-time">{calculateTimeLeft(timeLeft)}</div>
+			<button onClick={() => setIsPaused(!isPaused)}>
+				{isPaused ? 'START' : 'STOP'}
+			</button>
 			<h2>{`Correct keywords so far: ${correctWords.length}/${
 				allKeywords.length
 			}, Percentage: ${calculatePercentage({
